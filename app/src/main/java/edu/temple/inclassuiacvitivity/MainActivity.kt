414dcd2b2f11f0f57dcc181d77669cce4c2ccbff
thegreatandmightywafflecaptain <sirmadametheknight@gmail.com>
2024-02-06 11:53:25 -0500

    Correctly populated the numberArray

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.temple.inclassuiacvitivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.*
 
@@ -14,7 +15,16 @@ class MainActivity : AppCompatActivity() {
         val displayTextView = findViewById<TextView>(R.id.textDisplay)
 
         /* TODO Step 1: Populate this array */
-        //val numberArray = Array Of Numbers
+        var numberArray = arrayOf<Int>()
+        var counter = 0
+        for(i in 2..100){
+            if(i % 2 == 0){
+                //Log.d("Number", i.toString())
+                numberArray[counter] = i
+            }
+        }
+
+        Log.d("Output", numberArray.toString())
 
         /* TODO Step 2: Create adapter to display items from array in Spinner */
         //spinner.adapter = ArrayAdapter...
